fdc4d1f2db79ed0adc0528bf48262733483fd49e
Ug0510 <uditgg0510@gmail.com>
2023-07-13 01:53:59 +0530

    Adding Sorting feature in the result Table

--- Django/miniproject/votingapp/views.py
+++ Django/miniproject/votingapp/views.py
@@ -1,28 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-
-arr = ['Java','Cplusplus','Python','Chash','DotNet','PHP','SQL','Ruby','R']
+arr = ['Java', 'Cplusplus', 'Python', 'Chash', 'DotNet', 'PHP', 'SQL', 'Ruby', 'R']
 globalcnt = dict()
 
 # Create your views here.
 def index(request):
     mydictionary = {
-        'arr':arr
+        'arr': arr
     }
-    return render(request,'index.html',context=mydictionary)
+    return render(request, 'index.html', context=mydictionary)
 
 def getquery(request):
     q = request.GET['languages']
     if q in globalcnt:
-        #if already exists then increment the frequency
+        # if already exists then increment the frequency
         globalcnt[q] += 1
     else:
-        #first occurence
+        # first occurrence
         globalcnt[q] = 1
-    mydictionay = {
-        'arr':arr,
-        "globalcnt":globalcnt
+    
+    # sorting the dictionary in ascending order based on their counts
+    sorted_dict = {k: v for k, v in sorted(globalcnt.items(), key=lambda item: item[1])}
+
+    mydictionary = {
+        'arr': arr,
+        'globalcnt': sorted_dict
     }
-    return render(request,'index.html',context=mydictionay)
+    return render(request, 'index.html', context=mydictionary)

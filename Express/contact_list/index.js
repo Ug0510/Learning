98844d2c8bddabcd84ad1ed643ae12af086fa629
Ug0510 <uditgg0510@gmail.com>
2023-08-21 20:54:53 +0530

    Form added

--- Express/contact_list/index.js
+++ Express/contact_list/index.js
@@ -3,6 +3,20 @@ const path = require('path');
 const port = 8080;
 
 const app = express();
+const contactList = [
+    {
+        name:'Udit Gupta',
+        number:'1234567890'
+    },
+    {
+        name:'Raj Malhotra',
+        number:'9873213984'
+    },
+    {
+        name:'Vikas Dubey',
+        number:'8265923628'
+    }
+];
 
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname,'views'));
@@ -13,8 +27,15 @@ app.get('/',function(req,res){
 });
 
 app.get('/practise',function(req,res){
-    return res.render('practise',{title:'Playground'});
+    return res.render('practise',{
+        title:'Playground',
+        contact_list:contactList
+    });
 });
+
+app.post('/create-contact',function(req,res){
+    return res.redirect('/practise'); 
+})
 
 app.listen(port,function(err){
     if(err)

d1a6cbf0eaa75f946fbcda3b390a6090fdb898c2
Ug0510 <uditgg0510@gmail.com>
2023-08-21 17:23:49 +0530

    Sending data from controller to view to display

--- Express/contact_list/index.js
+++ Express/contact_list/index.js
@@ -5,11 +5,11 @@ const port = 8080;
 const app = express();
 
 app.set('view engine','ejs');
-// app.set('views', path.join(__dirname,'views'));
+app.set('views', path.join(__dirname,'views'));
 
 
 app.get('/',function(req,res){
-    return res.render('home');
+    return res.render('home',{title:'My Contact List'});
 });
 
 app.listen(port,function(err){
